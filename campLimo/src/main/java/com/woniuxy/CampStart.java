bf3b6121fa1c31341d11f12336d7dac28a337ceb
potentialshadow <1242152>
2020-11-07 14:23:58 +0800

    新增营地模块的yml和测试

--- campLimo/src/main/java/com/woniuxy/CampStart.java
+++ campLimo/src/main/java/com/woniuxy/CampStart.java
@@ -2,12 +2,17 @@ package com.woniuxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+//@MapperScan("com.woniuxy.cq.soft.mapper")
+//@ServletComponentScan("com.woniuxy.cq.soft.filter")
+//@ImportResource("classpath:transaction.xml")
+@EnableSwagger2
 public class CampStart
 {
     public static void main( String[] args )
