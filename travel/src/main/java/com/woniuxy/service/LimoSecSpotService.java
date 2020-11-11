6298ffd3a0d442223867fafb33df32b73274f1e0
xiaolili0303 <904187445@qq.com>
2020-11-11 14:13:39 +0800

    增加周边景点模块

--- travel/src/main/java/com/woniuxy/service/LimoSecSpotService.java
+++ travel/src/main/java/com/woniuxy/service/LimoSecSpotService.java
@@ -2,6 +2,8 @@ package com.woniuxy.service;
 
 import com.woniuxy.doman.LimoSecSpot;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.woniuxy.param.SortParam;
+import com.woniuxy.param.SortTypeParam;
 
 /**
  * <p>
@@ -12,5 +14,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-11-10
  */
 public interface LimoSecSpotService extends IService<LimoSecSpot> {
-
+    /**
+     * @Author zhuyuli
+     * @Description 条件查询周边景点
+     * @Date 2020/11/11 10:53
+     * @Param [param]
+     * @return java.lang.Object
+     **/
+    Object queryByParam(SortTypeParam param);
 }
